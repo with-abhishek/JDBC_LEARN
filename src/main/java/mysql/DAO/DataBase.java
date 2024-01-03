97e35c05f71467e2993f824aa58954af0321735f
withabhi <asp9514@gmail.com>
2024-01-03 23:25:21 +0530

    initial commit

---
+++ src/main/java/mysql/DAO/DataBase.java
@@ -0,0 +1,75 @@
+package mysql.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import mysql.entity.Employee;
+
+public class DataBase {
+	static Connection con;
+	static {
+	
+	try {
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mic", "root", "3121");
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+
+	}
+	
+	public void insert(Employee e)
+	{
+		try {
+
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(String.format("insert into Employee(id,name,gender,salary) values (%d,\"%s\",\"%s\",%d)",
+					e.getId(), e.getName(), e.getGender(), e.getSalary()));
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Mission successful.............");
+
+	}
+	
+	
+	public void showEmployees()
+	{
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from Employee");
+		
+		while(rs.next())
+		{
+			System.out.println("Id : "+ rs.getInt(1)+"  Name : "+ rs.getString(2)+ "  Gender : "+ rs.getString(3)+ "  Salary : "+ rs.getInt(4));
+		}
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public void insertByPrepare(Employee e)
+	{
+		try {
+
+			PreparedStatement ps = con.prepareStatement("insert into Employee(id,name,gender,salary) values (?,?,?,?)");
+			ps.setInt(1, e.getId());
+			ps.setString(2, e.getName());
+			ps.setString(3, e.getGender());
+			ps.setInt(4, e.getSalary());
+			
+			ps.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Mission successful by prep.............");
+
+	}
+}
---
+++ src/main/java/mysql/client/App.java
@@ -0,0 +1,23 @@
+package mysql.client;
+
+import mysql.DAO.DataBase;
+import mysql.entity.Employee;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		DataBase db = new DataBase();
+		Employee e = new Employee();
+		e.setId(2);
+		e.setName("Anup");
+		e.setGender("Male");
+		e.setSalary(90900);
+		
+		db.insertByPrepare(e);
+//		db.insert(e);
+		db.showEmployees();
+		
+	}
+
+}		
